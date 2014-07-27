e2686839321c259c4aba03d412eea4e910cc468a
Remco Coumou <remco.coumou@gmail.com>
2014-07-27 23:45:49 +0200

    Revert "Update"
    This reverts commit 7d9b5f327d623828cc87324285256fa9d49e9cb9.

--- gettingdata/project/run_analysis.R
+++ gettingdata/project/run_analysis.R
@@ -1,53 +1,60 @@
 # Coursera Data Science Project	-	Getting and Cleaning Data
+
+# Assignment:
+# You should create one R script called run_analysis.R that does the following:
+#	1. Merges the training and the test sets to create one data set.
+#	2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#	3. Uses descriptive activity names to name the activities in the data set
+#	4. Appropriately labels the data set with descriptive variable names. 
+#	5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+# Here is the original data set for this assignement:
+#	https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip [1]
+#	[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+# Download this file to your computer and unzip it in your working directory in R for this project
+
+
 # Author: R.Coumou
 # Date: 27-7-2014
 
-# Install the required packages if they are not yet installed:
-if (!require("data.table")) {
-  install.packages("data.table")
-}
-
-if (!require("reshape2")) {
-  install.packages("reshape2")
-}
-
-require("data.table")
-require("reshape2")
 
 # Read in the data sets and assign column names:
-activity		<- read.table("activity_labels.txt")
-    names(activity) <- c("id", "name")
-feature			<- read.table("features.txt")
-    names(feature) <- c("id", "name")
-x.train	 		<- read.table("./train/x_train.txt")
-y.train	 		<- read.table("./train/y_train.txt")
-x.test	 		<- read.table("./test/x_test.txt")
-y.test	 		<- read.table("./test/y_test.txt")
-subject.train	<- read.table("./train/subject_train.txt")
-subject.test 	<- read.table("./test/subject_test.txt")
 
+activity	<- read.table("activity_labels.txt")
+    names(activity) <- c("id", "name")
+
+feature	<- read.table("features.txt")
+    names(feature) <- c("id", "name")
+
+x.train	 <- read.table("./train/x_train.txt")
+y.train	 <- read.table("./train/y_train.txt")
+x.test	 <- read.table("./test/x_test.txt")
+y.test	 <- read.table("./test/y_test.txt")
+subject.train<- read.table("./train/subject_train.txt")
+subject.test <- read.table("./test/subject_test.txt")
 
 # Assign the features to the x-axis and the activities to the y-axis:
-names(x.train)	  	<- feature$name
-names(x.test)	  	<- feature$name
-names(y.train)	  	<- c("activities")
-names(y.test)	  	<- c("activities")
+names(x.train)	  <- feature$name
+names(x.test)	  <- feature$name
+names(y.train)	  <- c("activities")
+names(y.test)	  <- c("activities")
 names(subject.train)<- c("subject")
 names(subject.test) <- c("subject")
 
 
-# Merge the training and the test data sets:
-x 		<- rbind(x.train, x.test)
-y 		<- rbind(y.train, y.test)
+# Merge the data sets:
+x <- rbind(x.train, x.test)
+y <- rbind(y.train, y.test)
 subject <- rbind(subject.train, subject.test) 
 
 # Assign a readable name for the activities 
 y$activities <- activity[y$activities, ]$name
 
+
 # Extract only the mean and standard deviations for each measurement:
 z <- x[ , grep(".*mean\\(\\)|.*std\\(\\)", feature$name)]
 
-# Blend the data sets into ONE tidy data set that only contains the selected measurements:
+# Blend the data sets into a tidy one:
 data.set <- cbind(subject, y, z)
 
 # Write the tidy data set with the selected measurements to disk:
